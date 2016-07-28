3468a7694e16ff6f1e07c9447bd16fe600f076be
shonenada <shonenada@gmail.com>
2016-07-28 23:05:50 +0800

    Add account models

--- project.clj
+++ project.clj
@@ -6,11 +6,16 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.5"]
                  [org.clojure/tools.logging "0.3.0"]
+                 [org.clojure/java.jdbc "0.3.5"]
+                 [org.clojure/data.generators "0.1.2"]
+                 [mysql/mysql-connector-java "5.1.31"]
+                 [org.mindrot/jbcrypt "0.3m"]
                  [environ "0.5.0"]
                  [ring/ring-core "1.2.2"]
                  [ring/ring-json "0.3.1"]
                  [ring/ring-jetty-adapter "1.2.2"]
                  [compojure "1.5.0"]
+                 [korma "0.4.2"]
                  [selmer "1.0.7"]]
   :main ^:skip-aot cupid.core
   :profiles {:uberjar {:aot :all}})
---
+++ src/cupid/models/account.clj
@@ -0,0 +1,52 @@
+(ns cupid.models.account
+  (:use [korma.core]
+        [cupid.database])
+  (:require [cupid.utils.common :as utils]
+            [cupid.utils.cipher :as cipher]))
+
+(defn get-by-id [id]
+  (first (select accounts
+                 (where {:id id}))))
+
+(defn get-by-username [username]
+  (first (select accounts
+                 (where {:username username}))))
+
+(defn get-by-email [email]
+  (first (select accounts
+                 (where {:email email}))))
+
+(defn get-by-mobile [mobile]
+  (first (select accounts
+                 (where {:mobile mobile}))))
+
+(defn username-exist? [username]
+  (if-let [accounts (get-by-username username)]
+    (true)
+    (false)))
+
+(defn email-exist? [email]
+  (if-let [accounts (get-by-email email)]
+    (true)
+    (false)))
+
+(defn mobile-exist? [mobile]
+  (if-let [accounts (get-by-mobile mobile)]
+    (true)
+    (false)))
+
+(defn create [account]
+  (let [{id :generated_key} (insert accounts (values account))]
+    (get-by-id id)))
+
+(defn check-password [account raw-password]
+  (cipher/check-hash raw-password (:password account)))
+
+(defn set-token [account token]
+  (update accounts
+          (set-fields {:token token})
+          (where {:id {:id account}})))
+
+(defn check-token [account token]
+  (let [account-token {:token account}]
+    (= account-token token)))
