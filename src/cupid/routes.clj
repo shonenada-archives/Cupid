9524a7b911c037c12ef514d616edeb1485221faa
shonenada <shonenada@gmail.com>
2016-08-16 00:09:43 +0800

    view staff routes

--- src/cupid/routes.clj
+++ src/cupid/routes.clj
@@ -11,17 +11,17 @@
 (defroutes app-routes
   (GET "/" [request] index-controller/index)
 
-  (GET "/account/signup" [request] account-controller/signup-page)
-  (POST "/account/signup" [request] account-controller/signup)
+  (GET "/account/signup" [] account-controller/signup-page)
+  (POST "/account/signup" [] account-controller/signup)
 
-  (GET "/account/signin" [request] account-controller/signin-page)
-  (POST "/account/signin" [request] account-controller/signin)
+  (GET "/account/signin" [] account-controller/signin-page)
+  (POST "/account/signin" [] account-controller/signin)
 
-  (GET "/account/signout" [request] account-controller/signout)
+  (GET "/account/signout" [] account-controller/signout)
 
-  (GET "/staffs" [request] staff-controller/staffs)
-  (POST "/staffs" [request] staff-controller/create-staff)
-  (GET "/staffs/:id" [request id] staff-controller/view-staff)
+  (GET "/staffs" [] staff-controller/staffs)
+  (POST "/staffs" [] staff-controller/create-staff)
+  (GET "/staffs/:id" [request id] (staff-controller/view-staff request id))
 
   (route/resources "/")
   (route/not-found (page-not-found)))
